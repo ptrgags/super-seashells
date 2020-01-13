00546371f013a83e204c7f14d69a3cd57ddf44e9
Peter Gagliardi <ptrgags@gmail.com>
2020-01-12 19:32:49 -0500

    simplify command line usage

--- main.py
+++ main.py
@@ -4,22 +4,24 @@ from argparse import ArgumentParser
 
 from superseashells import SuperSeashell
 
-def fetch_params(args):
-    with open(args.json_fname, 'r') as f:
+def fetch_params(json_fname):
+    with open(json_fname, 'r') as f:
         return json.load(f)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument(
-        'json_fname', 
-        help='JSON file with parameters for the superseashell')
-    parser.add_argument(
-        'obj_fname',
-        help='output OBJ file name to store the result')
+        'shape_id', 
+        help=(
+            'Name of the shape. This will be used to read'
+            'params/<shape_id>.json and write models/<shape_id>.obj'))
     args = parser.parse_args()
 
-    shell = SuperSeashell(fetch_params(args))
+    json_fname = f'params/{args.shape_id}.json'
+    shell = SuperSeashell(fetch_params(json_fname))
     mesh = shell.generate_mesh()
 
-    with open(args.obj_fname, 'w') as f:
+
+    obj_fname = f'models/{args.shape_id}.obj'
+    with open(obj_fname, 'w') as f:
         mesh.write_obj(f)
