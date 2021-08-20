bb9747816439e99de5b2c331e0a553d2442d5b08
Peter Gagliardi <ptrgags@gmail.com>
2021-08-20 07:35:09 -0400

    Get a helix working

--- diffyshell/DifferentialPath.py
+++ diffyshell/DifferentialPath.py
@@ -3,16 +3,18 @@ import numpy
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
+numpy.set_printoptions(suppress=True)
+
 class DifferentialPath():
     def __init__(self):
         # Current arc length
         self.arc_length = 0
         # Arc length per iteration
-        self.delta_arc_length = 0.1
+        self.delta_arc_length = 0.01
         # Iterations per row
-        self.iters_per_row = 10
+        self.iters_per_row = 100
         # Total length of the path
-        self.max_length = 1000
+        self.max_length = 100
 
         # compute the number of rows we'll need for the simulation
         rows = math.ceil(
@@ -23,17 +25,25 @@ class DifferentialPath():
         self.positions = numpy.zeros((rows, 3))
 
         # Computed Frenet-Serret frame at the start of each row
-        self.tangents = numpy.zeros((rows, 3))
-        self.normals = numpy.zeros((rows, 3))
-        self.binormals = numpy.zeros((rows, 3))
+        self.tangents = numpy.zeros((rows, 3), dtype=numpy.float64)
+        self.normals = numpy.zeros((rows, 3), dtype=numpy.float64)
+        self.binormals = numpy.zeros((rows, 3), dtype=numpy.float64)
 
         # Computed curvature and torsion values at the start of each row
-        self.curvature = numpy.zeros(rows)
-        self.torsion = numpy.zeros(rows)
+        self.curvature = numpy.zeros(rows, dtype=numpy.float64)
+        self.torsion = numpy.zeros(rows, dtype=numpy.float64)
 
         # curvature and torsion callbacks
-        self.curvature_func = lambda s: 0.01
-        self.torsion_func = lambda s: 0.01
+        HELIX_A = 4
+        HELIX_B = 3
+        HELIX_DENOM = 25
+        HELIX_DENOM_LEN = 5
+        self.initial_position = [HELIX_A, 0, 0]
+        self.initial_tangent = [0, HELIX_A / HELIX_DENOM_LEN, HELIX_B / HELIX_DENOM_LEN]
+        self.initial_normal = [-1, 0, 0]
+        self.initial_binormal = [0, -HELIX_B / HELIX_DENOM_LEN, HELIX_A / HELIX_DENOM_LEN]
+        self.curvature_func = lambda s: HELIX_A / HELIX_DENOM
+        self.torsion_func = lambda s: HELIX_B / HELIX_DENOM
 
     def compute(self):
         """
@@ -60,10 +70,10 @@ class DifferentialPath():
         Initialize the first row of the simulation to initial
         values
         """
-        self.positions[0] = [1, 0, 0]
-        self.tangents[0] = [1, 0, 0]
-        self.normals[0] = [0, 1, 0]
-        self.binormals[0] = [0, 0, 1]
+        self.positions[0] = self.initial_position
+        self.tangents[0] = self.initial_tangent
+        self.normals[0] = self.initial_normal
+        self.binormals[0] = self.initial_binormal
 
         self.curvature[0] = self.curvature_func(0.0)
         self.torsion[0] = self.torsion_func(0.0)
@@ -91,10 +101,8 @@ class DifferentialPath():
         self.normals[row] = self.normals[prev_row]
         self.binormals[row] = self.binormals[prev_row]
 
-        # currently we are at row i.
-        # iterate a number of times and populate row i + 1
-        # then set the row to i + 1
-        for i in range(self.iters_per_row):
+        # iterate a number of times, updating the current row in place
+        for _ in range(self.iters_per_row):
             self.compute_iter(row)
     
     def compute_iter(self, row):
@@ -111,9 +119,6 @@ class DifferentialPath():
         - self.arc_length is incremented by self.delta_arc_length
         - variables[row] are updated in place
         """
-        # increment the arc length first to be the arc length
-        # at this iteration.
-        self.arc_length += self.delta_arc_length
 
         # compute the current curvature and torsion values
         s = self.arc_length
@@ -122,17 +127,31 @@ class DifferentialPath():
         self.curvature[row] = curvature
         self.torsion[row] = torsion
 
+        # increment the arc length
+        self.arc_length += self.delta_arc_length
+
         # get the current values since we'll be overwriting them
         position = self.positions[row]
         tangent = self.tangents[row]
         normal = self.normals[row]
         binormal = self.binormals[row]
 
+        '''
+        print("iter")
+        print(position, tangent, normal, binormal)
+        print(curvature, torsion)
+        '''
+
         # compute the changes in the Frenet-Serret frame directions given
         # the Frenet-Serret formulas
         delta_tangent = curvature * normal
         delta_normal = -curvature * tangent + torsion * binormal
         delta_binormal = -torsion * normal
+
+        '''
+        print("deltas")
+        print(delta_tangent, delta_normal, delta_binormal)
+        '''
 
         # use Euler integration to update the Frenet-Serret frame.
         self.tangents[row] = tangent + self.delta_arc_length * delta_tangent
